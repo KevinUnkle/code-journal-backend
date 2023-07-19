8376370b5c576cd6e522f0aea391de891e386484
Kevin Unkle <kevinunk@gmail.com>
2023-07-19 18:35:31 +0000

    Issue 1 on its way

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 import 'dotenv/config';
 import pg from 'pg';
 import express from 'express';
+import ClientError from './lib/client-error.js';
+import errorMiddleware from './lib/error-middleware.js';
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
@@ -14,4 +16,38 @@ const app = express();
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
+});
+
+app.get('/api/entries', async (req, res, next) => {
+  try {
+    const sql = `
+      select *
+        from "entries"
+        order by "entryId"
+    `;
+    const result = await db.query(sql);
+    res.json(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/api/entries', async (req, res, next) => {
+  try {
+    const { title, notes, photoUrl } = req.body;
+    if (!title || !notes || !photoUrl) {
+      throw new ClientError(400, 'title, notes, photo URL required');
+    }
+    const sql = `
+      insert into "entries" ("title", "notes", "photoUrl")
+        values ($1, $2, $3)
+        returning *
+    `;
+    const params = [title, notes, photoUrl];
+    const result = await db.query(sql, params);
+    const [entry] = result.rows;
+    res.status(201).json(entry);
+  } catch (err) {
+    next(err);
+  }
 });
